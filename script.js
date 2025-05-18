4cc76e0f746f894156ca7310e1083704330a61d8
Casey <soulcasey@gmail.com>
2025-05-17 20:18:31 -0400

    Remove change title, fix toggle shift

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const words = ["Software", "Game", "DevOps", "Network"];
-let index = 0;
-const textElement = document.querySelector(".changing-text");
-
-function changeText() {
-	textElement.style.opacity = 0; // Fade out
-
-	setTimeout(() => {
-		index = (index + 1) % words.length;
-		textElement.textContent = words[index];
-		textElement.style.opacity = 1; // Fade in
-	}, 500);
-}
-
-// First change after 0.5 seconds
-setTimeout(() => {
-	changeText();
-	setInterval(changeText, 2500); // Then every 2.5 seconds
-}, 1000);
-
 const cards = document.querySelectorAll('.project-card');
 const modal = document.getElementById('modal');
 const embed = document.getElementById('modal-embed');
